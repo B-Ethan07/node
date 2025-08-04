1b4eb86f665a18b48d979a7176645bbcf97d66c8
Ethan B <e.boulard07@gmail.com>
2025-08-04 11:07:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+
+console.log(
+  chalk.blue("Hello Myra!")
+);
+console.log(
+  chalk.yellow("Hello Ethan!")
+);
+console.log(
+  chalk.green("Hello Aline!")
+);
+console.log(
+  chalk.red("Hello Shalom!")
+);
